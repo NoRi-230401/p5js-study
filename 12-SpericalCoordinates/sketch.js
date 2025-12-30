411099a0cdb20674e9389fb5a4622e0ae2730197
NoRi-230401 <sedia2011-apple@yahoo.co.jp>
2025-12-30 09:14:36 +0900

    v002

--- 12-SpericalCoordinates/sketch.js
+++ 12-SpericalCoordinates/sketch.js
@@ -1,69 +1,57 @@
-let r = 0;
-let axisLen;
-let thetaMaxSlider, phiMaxSlider;
-let densitySlider;
-let radiusSlider;
-let radius;
-let thetaMax, phiMax;
-let density;
+let r;
+let radiusSlider,thetaMaxSlider, phiMaxSlider,densitySlider;
+let radius,thetaMax, phiMax, density;
 
 function setup() {
-  createCanvas(600, 600, WEBGL);
+  createCanvas(700, 700, WEBGL);
   angleMode(DEGREES);
-  colorMode(HSB);
   noFill();
-
-  r = width / 4;
-  axisLen = width / 2.6;
 
   //Create slider!
   radius = createDiv();
   radius.class("valueDisplay");
-  radiusSlider = createSlider(0, width / 3, width / 4, 1);
+  radiusSlider = createSlider(0, width / 2.5, width / 3, 1);
   radiusSlider.class("Slider");
 
   phiMax = createDiv();
   phiMax.class("valueDisplay");
-  phiMaxSlider = createSlider(0, 180, 180, 5);
+  phiMaxSlider = createSlider(0, 180, 180, 1);
   phiMaxSlider.class("Slider");
 
   thetaMax = createDiv();
   thetaMax.class("valueDisplay");
-  thetaMaxSlider = createSlider(0, 360, 360, 10);
+  thetaMaxSlider = createSlider(0, 360, 360, 1);
   thetaMaxSlider.class("Slider");
 
   density = createDiv();
   density.class("valueDisplay");
-  densitySlider = createSlider(13, 72, 36, 1);
+  densitySlider = createSlider(6, 90, 36, 1);
   densitySlider.class("Slider");
 }
 
 function draw() {
   background(0);
   orbitControl();
-  rotateY(frameCount * -0.1);
 
-  stroke(321, 38, 80);
-  strokeWeight(2);
+  strokeWeight(1);
+  stroke("yellow");
+  debugMode(r * 2, 8, 0, 0, 0, r / 5, 0, -r, 0);
+
+  push();
   rotateZ(23.4);
-  rotateX(-30);
-  rotateY(-30);
+  rotateY(frameCount * 0.25);
 
-  // rotateX(-30);
-  // rotateY(-30);
   r = radiusSlider.value();
 
-  for (
-    let phi = 0;
-    phi < phiMaxSlider.value();
-    phi += 360 / densitySlider.value()
-  ) {
+  strokeWeight(1);
+  stroke("orange");
+  line(0, r * 1.2, 0, 0, -r * 1.2, 0); // y-axis
+
+  stroke("skyblue");
+  strokeWeight(3);
+  for (let phi = 0; phi < phiMaxSlider.value(); phi += 360 / densitySlider.value()) {
     beginShape(POINTS);
-    for (
-      let theta = 0;
-      theta < thetaMaxSlider.value();
-      theta += 360 / densitySlider.value()
-    ) {
+    for (let theta = 0;theta < thetaMaxSlider.value(); theta += 360 / densitySlider.value()) {
       let y = -r * cos(phi);
       let x = r * sin(phi) * sin(theta);
       let z = r * sin(phi) * cos(theta);
@@ -72,17 +60,10 @@ function draw() {
     endShape(CLOSE);
   }
 
-  strokeWeight(5);
-  stroke("red");
-  line(0, 0, 0, axisLen, 0, 0); // x-axis
-  stroke("green");
-  line(0, 0, 0, 0, -axisLen, 0); // y-axis
-  stroke("blue");
-  line(0, 0, 0, 0, 0, axisLen); // z-axis
+  pop();
 
-  thetaMax.html("theta max value: " + thetaMaxSlider.value());
-  phiMax.html("phi max value: " + phiMaxSlider.value());
-  radius.html("radius value: " + radiusSlider.value());
-  let mappedDensity = int(map(densitySlider.value(), 13, 72, 1, 60));
-  density.html("Density: " + mappedDensity);
+  radius.html("radius: " + radiusSlider.value());
+  thetaMax.html("theta max: " + thetaMaxSlider.value());
+  phiMax.html("phi max: " + phiMaxSlider.value());
+  density.html("Density: " + densitySlider.value());
 }
